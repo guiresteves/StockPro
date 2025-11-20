8c698ea3cf8a20ff77f5f8c50e599c68bb092ce1
Guilherme Esteves <guiresteves@gmail.com>
2025-11-20 02:47:29 -0300

    feat: Inicializando o projeto

---
+++ src/main/java/com/br/stockpro/StockproApplication.java
@@ -0,0 +1,13 @@
+package com.br.stockpro;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StockproApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StockproApplication.class, args);
+	}
+
+}
